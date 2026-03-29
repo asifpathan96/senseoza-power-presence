a438ff133e993e593b1400145d9b2ee0fb94e315
asifpathan96 <asif2apathan@gmail.com>
2026-03-30 02:30:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
+  base: "./",
   server: {
     host: "::",
     port: 8080,
